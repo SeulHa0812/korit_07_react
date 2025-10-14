0d1abe0813dcc2a7eb44c7e0fc2a17c4a3a4cff2
SeulHa0812 <haseull0812@gmail.com>
2025-10-14 16:38:52 +0900

    feat: React lesson started

---
+++ myapp/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Hello from "./Hello"
+import Hello2 from './Hello2'
+import Drink from './Drink'
+import MyComponent2 from './MyComponent2'
+import MyComponent from './MyComponent'
+import './App.css'
+import Counter from './Counter'
+
+function App() {
+
+  return (
+    <>
+      <Counter />
+    </>
+  )
+}
+
+export default App
---
+++ myapp/src/Counter.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function Counter() {
+  // 초기값이 0인 상태를 선언.
+  const [count, setCount] = useState(0); //1. 최상위에서 호출
+
+  return (
+    <div>
+      <p>버튼 클릭 횟수 = {count}</p>
+      <button onClick={() => setCount(count + 1)}>
+        증가 {count}
+      </button>
+    </div>
+  );
+
+}
+
+export default Counter;
